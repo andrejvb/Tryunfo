b2995aa131454b2e714361f297a3e2a6f98f3b0c
andrejvb <andre.jvb@gmail.com>
2022-12-13 14:11:33 -0300

    requisito 01

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import propTypes from 'prop-types'
+import Form from './components/Form';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <>
         <h1>Tryunfo</h1>
-      </div>
+        <Form />
+      </>
     );
   }
 }
